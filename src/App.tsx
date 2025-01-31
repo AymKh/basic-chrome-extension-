1ebbc1d414db8e8977c7fefdafb4299fb3f16d78
Aymen KHEDHRIYA <aymen.khedhriya@gmail.com>
2025-01-31 22:07:16 +0100

    feat: basic popup

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,9 @@ import './App.css'
 
 function App() { // start bilding your popup app here 
   return (
-    <h1 className="text-3xl text-center font-bold underline">
-      Hello world!
+    <h1 className="flex flex-col items-center justify-center text-3xl font-bold text-center underline w-[300px] h-[400px]">
+      <h1>TIS BUT A SCRATCH</h1>
+      <img src="https://i.pinimg.com/originals/a2/bc/e2/a2bce2d827b4f4f7c8a56189bc2bf09e.gif" alt="tis but a scratch gif" />
     </h1>
   )
 }
